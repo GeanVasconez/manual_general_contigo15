73746d22b4fcf4baaa02e911b5164d070c12213a
Gean Vasconez <gean.vasconez@manexware.com>
2023-09-15 16:01:46 -0500

    add css file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,10 +29,10 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
-html_css_files = [
-    'css/contigo.css',
-],
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+
+def setup(app):
+    app.add_css_file('css/contigo.css')
